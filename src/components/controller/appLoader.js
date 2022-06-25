d2845c99536cd5b74416979ea282f18f7fb434f0
Asya19 <asya.kisko@yandex.ru>
2022-06-25 20:59:44 +0300

    feat: add API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '0595532984a441b88e978775d3e20333', // получите свой ключ https://newsapi.org/
         });
     }
 }
